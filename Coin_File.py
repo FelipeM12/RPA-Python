d1a6940008c2bfb2980bf33d513a89e372991009
Felipe Maciel <felipe56.fm@gmail.com>
2023-06-21 15:02:01 -0300

    Projeto 0.1

--- Coin_File.py
+++ Coin_File.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import xlsxwriter
 
-#Criar e salvar arquivo com os parametros de "Nome do arquivo(filename) e Dataframe(resultTable)"
+'''Criar e salvar arquivo com os parametros de "Nome do arquivo(filename) e Dataframe(resultTable)"'''
+
+
+class TableCoin:
+   def __init__(self, moeda_1, moeda_2):
+      self.moeda1 = moeda_1
+      self.moeda2 = moeda_2      
+
+
 def saveFile(resultTable, filename):
    writerTable = pd.ExcelWriter(filename, engine='xlsxwriter')
    resultTable.to_excel(writerTable, sheet_name='Coins', index=False)
